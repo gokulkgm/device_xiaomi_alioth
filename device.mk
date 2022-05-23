166d1695d06bc4ddef4e9943bdab2396d589446e
Gokul GK <gk@gkannan.dev>
2022-05-23 21:30:49 +0530

    Test: Revert "rip auto refresh rate"
    This reverts commit 5aa343a16b71de97cadc6c8f57452181599cd288.

--- device.mk
+++ device.mk
@@ -170,6 +170,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.use_smooth_motion=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=1500 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.defer_fps_frame_count=2
 
 PRODUCT_COPY_FILES += \
